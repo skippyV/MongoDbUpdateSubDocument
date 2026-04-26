af4f3f2fc4d7d5c61e04b1f3605ed56350c7b4a8
skippyV <skippyV@users.noreply.github.com>
2026-04-26 11:18:13 -0400

    Added AddToSet() calls

--- UpdateSubDocument/FifthTests.cs
+++ UpdateSubDocument/FifthTests.cs
@@ -63,6 +63,55 @@ namespace UpdateSubDocument
             Console.WriteLine($"ModifiedCount: {result.ModifiedCount}");
             iLogger.LogInformation($"MatchedCount: {result.MatchedCount}");
             iLogger.LogInformation($"ModifiedCount: {result.ModifiedCount}");
+
+            // NOW TO test AddToSet()
+            iLogger.LogInformation(" FifthTests - NOW TO test AddToSet()");
+
+            filterTeam = Builders<Team>.Filter.Eq("Id", "000000000000000000001000");
+
+            // updateStringFilter = $"Players.$[{identifier1}].Questions.$[{identifier2}].ImageFileIds.$[{identifier3}]";
+            updateStringFilter = $"Players.$[{identifier1}].Questions.$[{identifier2}].ImageFileIds";
+
+            updateUsingArrayFilter = Builders<Team>.Update.AddToSet(updateStringFilter, "xxyyZZ");
+
+            arrayFilter1 = new BsonDocumentArrayFilterDefinition<BsonDocument>(
+                new BsonDocument($"{identifier1}._id", new BsonDocument("$eq", ObjectId.Parse("000000000000000000001001")))
+                );
+
+            arrayFilter2 = new BsonDocumentArrayFilterDefinition<BsonDocument>(
+                new BsonDocument($"{identifier2}.Number", 2));
+
+            //arrayFilter3 = new BsonDocumentArrayFilterDefinition<BsonDocument>(
+            //    new BsonDocument($"{identifier3}", "004"));
+
+            arrayFilters = new List<ArrayFilterDefinition> { 
+                arrayFilter1
+                ,arrayFilter2
+             //   ,arrayFilter3 
+            };
+
+            updateOptions = new UpdateOptions
+            {
+                ArrayFilters = arrayFilters
+            };
+
+            iLogger.LogInformation("Calling UpdateOne to update THIRD Level using AddToSet()");
+            result = collection.UpdateOne(filterTeam, updateUsingArrayFilter, updateOptions);
+
+            Console.WriteLine($"MatchedCount: {result.MatchedCount}");
+            Console.WriteLine($"ModifiedCount: {result.ModifiedCount}");
+            iLogger.LogInformation($"MatchedCount: {result.MatchedCount}");
+            iLogger.LogInformation($"ModifiedCount: {result.ModifiedCount}");
+
+            iLogger.LogInformation(" FifthTests - calling AddToSet() a second time - should be no change");
+            Console.WriteLine(" FifthTests - calling AddToSet() a second time - should be no change");
+            result = collection.UpdateOne(filterTeam, updateUsingArrayFilter, updateOptions);
+
+            Console.WriteLine($"MatchedCount: {result.MatchedCount}");
+            Console.WriteLine($"ModifiedCount: {result.ModifiedCount}");
+            iLogger.LogInformation($"MatchedCount: {result.MatchedCount}");
+            iLogger.LogInformation($"ModifiedCount: {result.ModifiedCount}");
+
         }
     }
 
